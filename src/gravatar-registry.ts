3135a2c01e6a23820045547d9c7516e8443133c2
Vince Laird <vincelaird@gmail.com>
2022-12-30 09:28:34 -0800

    first commit

--- src/gravatar-registry.ts
+++ src/gravatar-registry.ts
@@ -3,32 +3,37 @@ import {
   GravatarRegistry,
   NewGravatar,
   UpdatedGravatar
-} from "../generated/GravatarRegistry/GravatarRegistry"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/Gravity/GravatarRegistry"
+import { Gravatar } from "../generated/schema"
 
 export function handleNewGravatar(event: NewGravatar): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+    // entity.count = BigInt.fromI32(0)
+  // }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+
+  // entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity.id = event.params.id
-  entity.owner = event.params.owner
+
+  // entity.id = event.params.id
+  // entity.owner = event.params.owner
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+
+  // entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -49,6 +54,40 @@ export function handleNewGravatar(event: NewGravatar): void {
   // - contract.gravatars(...)
   // - contract.gravatarToOwner(...)
   // - contract.ownerToGravatar(...)
+
+  // Use id field from emitted event as unique id for the entity
+  const id = event.params.id.toHex();
+
+  // Create a new Gravatar Entity with unique id
+  const gravatar = new Gravatar(id);
+
+  // Set Gravatar Entity fields
+  gravatar.owner = event.params.owner;
+  gravatar.displayName = event.params.displayName;
+  gravatar.imageUrl = event.params.imageUrl;
+
+  // Save entity to store
+  gravatar.save();
+
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {}
+export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+  // Use proper id to load an entity from store
+  const id = event.params.id.toHex();
+
+  // Load the entity to be updated
+  let gravatar = Gravatar.load(id);
+
+  // Create the entity if it doesn't already exist
+  if (!gravatar) {
+    gravatar = new Gravatar(id);
+  }
+
+  // Set updated fields to entity
+  gravatar.owner = event.params.owner;
+  gravatar.displayName = event.params.displayName;
+  gravatar.imageUrl = event.params.imageUrl;
+
+  // Save updated entity to store
+  gravatar.save();  
+}
